69c78f35479b653d684f37942780466a799a231c
Aaron Powell <me@aaron-powell.com>
2012-12-02 20:44:58 +1100

    initial commmit

---
+++ TaskBoard/js/default.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Binding.optimizeBindingReferences = true;
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+            } else {
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.start();
+})();
