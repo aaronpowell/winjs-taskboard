3f3c4b11f1182ed8fe2d8949acd23dced3208024
Aaron Powell <me@aaron-powell.com>
2012-12-06 15:04:19 +1100

    Moving appbar into its own partial

---
+++ TaskBoard/pages/overview/appbar.js
@@ -0,0 +1,93 @@
+﻿require.define('/pages/overview/appbar.js', function (require, module, exports) {
+    var Presenter = require('Presenter');
+
+    var AppBarViewModel = function (context, element) {
+        var db = context.db;
+        var deleteHandler = function () {
+            var list = element.querySelector('.list').winControl;
+            var item = list.currentItem;
+            item = list.itemDataSource.list.getAt(item.index);
+
+            var transaction = db.transaction('task', 'readwrite');
+            var store = transaction.objectStore('task');
+
+            store.delete(item.id).onsuccess = function () {
+                list.itemDataSource.remove(list.currentItem.key);
+            };
+        };
+        var completeHandler = function () {
+            var list = element.querySelector('.list').winControl;
+            var item = list.currentItem;
+            item = list.itemDataSource.list.getAt(item.index);
+            item.completed = true;
+            item.done = true;
+
+            var transaction = db.transaction('task', 'readwrite');
+            var store = transaction.objectStore('task');
+
+            store.put(item).onsuccess = function () {
+                list.itemDataSource.remove(list.currentItem.key);
+            };
+        };
+        var pinHandler = function () {
+            var list = element.querySelector('.list').winControl;
+            var item = list.currentItem;
+            item = list.itemDataSource.list.getAt(item.index);
+            item.pinned = this.winControl.icon === WinJS.UI.AppBarIcon.pin;
+
+            var transaction = db.transaction('task', 'readwrite');
+            var store = transaction.objectStore('task');
+
+            store.put(item).onsuccess = function () {
+                var tile = new Windows.UI.StartScreen.SecondaryTile('item.' + item.id);
+                if (item.pinned) {
+
+                    tile.foregroundText = Windows.UI.StartScreen.ForegroundText.dark;
+                    tile.backgroundColor = Windows.UI.Colors.darkRed;
+                    tile.shortName = tile.displayName = item.title;
+                    tile.arguments = JSON.stringify({
+                        id: item.id
+                    });
+                    tile.logo = new Windows.Foundation.Uri("ms-appx:///images/logo.png");
+
+                    tile.requestCreateAsync();
+                } else {
+                    tile.requestDeleteAsync();
+                }
+            };
+        };
+
+        var presenter = new Presenter({
+            element: element,
+            ui: {
+                commands: [{
+                    id: 'addCommand',
+                    icon: WinJS.UI.AppBarIcon.add,
+                    label: 'Add',
+                    onclick: function () {
+                        context.app.setLocation('#/create');
+                    }
+                }, {
+                    id: 'deleteCommand',
+                    icon: WinJS.UI.AppBarIcon.delete,
+                    label: 'Delete',
+                    section: 'selection',
+                    onclick: deleteHandler
+                }, {
+                    id: 'completeCommand',
+                    icon: WinJS.UI.AppBarIcon.accept,
+                    label: 'Complete',
+                    section: 'selection',
+                    onclick: completeHandler
+                }, {
+                    icon: WinJS.UI.AppBarIcon.pin,
+                    id: 'pinCommand',
+                    label: 'Pin',
+                    onclick: pinHandler
+                }]
+            }
+        });
+    };
+
+    module.exports = AppBarViewModel;
+});
--- TaskBoard/pages/overview/index.js
+++ TaskBoard/pages/overview/index.js
@@ -3,99 +3,15 @@
     var setOptions = require('WinJS/UI').setOptions;
     var List = require('WinJS/Binding/List');
     var Presenter = require('Presenter');
+    var AppBarViewModel = require('/pages/overview/appbar.js');
 
     var OverviewPageViewModel = function (element) {
-        var context = this;
-        var db = context.db;
-        var deleteHandler = function () {
-            var list = element.querySelector('.list').winControl;
-            var item = list.currentItem;
-            item = list.itemDataSource.list.getAt(item.index);
+        var appBarViewModel = new AppBarViewModel(this, element);
 
-            var transaction = db.transaction('task', 'readwrite');
-            var store = transaction.objectStore('task');
-
-            store.delete(item.id).onsuccess = function () {
-                list.itemDataSource.remove(list.currentItem.key);
-            };
-        };
-        var completeHandler = function () {
-            var list = element.querySelector('.list').winControl;
-            var item = list.currentItem;
-            item = list.itemDataSource.list.getAt(item.index);
-            item.completed = true;
-            item.done = true;
-
-            var transaction = db.transaction('task', 'readwrite');
-            var store = transaction.objectStore('task');
-
-            store.put(item).onsuccess = function () {
-                list.itemDataSource.remove(list.currentItem.key);
-            };
-        };
-        var pinHandler = function () {
-            var list = element.querySelector('.list').winControl;
-            var item = list.currentItem;
-            item = list.itemDataSource.list.getAt(item.index);
-            item.pinned = this.winControl.icon === WinJS.UI.AppBarIcon.pin;
-
-            var transaction = db.transaction('task', 'readwrite');
-            var store = transaction.objectStore('task');
-
-            store.put(item).onsuccess = function () {
-                var tile = new Windows.UI.StartScreen.SecondaryTile('item.' + item.id);
-                if (item.pinned) {
-
-                    tile.foregroundText = Windows.UI.StartScreen.ForegroundText.dark;
-                    tile.backgroundColor = Windows.UI.Colors.darkRed;
-                    tile.shortName = tile.displayName = item.title;
-                    tile.arguments = JSON.stringify({
-                        id: item.id
-                    });
-                    tile.logo = new Windows.Foundation.Uri("ms-appx:///images/logo.png");
-
-                    tile.requestCreateAsync();
-                } else {
-                    tile.requestDeleteAsync();
-                }
-            };
-        };
-
-        var presenter = new Presenter({
-            element: element,
-            ui: {
-                commands: [{
-                    id: 'addCommand',
-                    icon: WinJS.UI.AppBarIcon.add,
-                    label: 'Add',
-                    onclick: function () {
-                        context.app.setLocation('#/create');
-                    }
-                }, {
-                    id: 'deleteCommand',
-                    icon: WinJS.UI.AppBarIcon.delete,
-                    label: 'Delete',
-                    section: 'selection',
-                    onclick: deleteHandler
-                }, {
-                    id: 'completeCommand',
-                    icon: WinJS.UI.AppBarIcon.accept,
-                    label: 'Complete',
-                    section: 'selection',
-                    onclick: completeHandler
-                },{
-                    icon: WinJS.UI.AppBarIcon.pin,
-                    id: 'pinCommand',
-                    label: 'Pin',
-                    onclick: pinHandler
-                }]
-            }
-        });
-
-        ready.call(this, presenter.element);
+        ready.call(this, element);
 
         //TODO: work out why this hack is required
-        presenter.element.querySelector('.list').style.height = '900px';
+        element.querySelector('.list').style.height = '900px';
     };
 
     var commands = [];
