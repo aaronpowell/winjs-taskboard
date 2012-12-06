1584d692c01bdc561185637f32579fe553294cd6
Aaron Powell <me@aaron-powell.com>
2012-12-06 17:49:10 +1100

    Moving Create to using a POST route

--- TaskBoard/pages/create/create.js
+++ TaskBoard/pages/create/create.js
@@ -9,33 +9,26 @@
         var presenter = new Presenter({
             element: element
         });
-
-        ready.call(context, presenter.element);
     };
 
     app.get('#/create', '/pages/create/create.template', CreatePageViewModel);
 
-    var ready = function (element) {
-        var context = this;
-        var db = context.db;
-        element.querySelector('form').addEventListener('submit', function (e) {
-            e.preventDefault();
+    app.post('#/create', function(context) {
+        var transaction = this.db.transaction('task', 'readwrite');
+        var store = transaction.objectStore('task');
 
-            var transaction = db.transaction('task', 'readwrite');
-            var store = transaction.objectStore('task');
-
-            var req = store.add({
-                title: element.querySelector('input[type="text"]').value,
-                description: element.querySelector('textarea').value,
-                due: element.querySelector('#due').winControl.current.getTime(),
-                completed: element.querySelector('#completed').winControl.checked,
-                done: element.querySelector('#completed').winControl.checked ? 'yes' : 'no',
-                created: Date.now()
-            });
-
-            req.onsuccess = function () {
-                context.app.setLocation('#/');
-            };
+        var req = store.add({
+            title: context.params['title'],
+            description: context.params['description'],
+            due: context.params['due'].getTime(),
+            completed: context.params['completed'],
+            done: context.params['completed'] ? 'yes' : 'no',
+            updated: Date.now(),
+            created: Date.now()
         });
-    };
+        
+        req.onsuccess = function (e) {
+            context.app.setLocation('#/');
+        };
+    });
 });
